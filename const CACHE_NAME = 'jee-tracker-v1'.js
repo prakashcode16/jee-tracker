4fd2766e39190d1648bcfe4c563f7b22550f0a45
prakashcode16 <prakashisprakash1602@gmail.com>
2026-09-16 21:11:09 +0530

    Update const CACHE_NAME = 'jee-tracker-v6'.js

--- const CACHE_NAME = 'jee-tracker-v1'.js
+++ const CACHE_NAME = 'jee-tracker-v1'.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jee-tracker-v5';
+const CACHE_NAME = 'jee-tracker-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
